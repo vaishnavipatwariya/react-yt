aab0ba7068303990c5525b373052508d52494545
vaishnavi patwariya <vaishnavipatwariya369@gmail.com>
2026-05-28 11:45:42 +0530

    Added 05 tailwind project

---
+++ 05-tailwind/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+const App = () => {
+  return (
+    <div className='bg-gray-700 h-screen flex items-center justify-center font-black'>
+      App
+    </div>
+  )
+}
+
+export default App
